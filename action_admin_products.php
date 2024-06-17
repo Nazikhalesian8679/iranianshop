8549432e4bed2a492618dc4f5af55ce480c4a969
Nazanin <nazanin.kh867913@gmail.com>
2024-06-17 08:14:09 -0700

    edit folder

---
+++ action_admin_products.php
@@ -0,0 +1,16 @@
+<?php
+include("includes/footer.php");
+if(!($_SESSION["state_login"]) && $_SESSION["state_login"]===true && $_SESSION["user_type"]=="admin")
+    {
+?>
+<<script type="text/javascript">
+    <!--
+    location.replace("index.php")
+    -->
+</script>
+<?php
+    }
+?>
+<?php
+if(isset($_POST['pro_code']) && !empty($_POST['$pro_code'])
+&& isset($_POST[]) )
